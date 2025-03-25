88ed1a532aa421007c426f2b8019b1d5bc7880b6
FlameC <91359913+FlameChart@users.noreply.github.com>
2025-03-25 22:18:52 +0800

    build: update dependencies.

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
-def hello_world():
+def index():
     return jsonify({'message': 'Hello World'})
 
 if __name__ == '__main__':
